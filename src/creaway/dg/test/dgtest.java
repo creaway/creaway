abc780d386c44d0af57c921c935adc8d84671609
creaway <261125798@qq.com>
2016-10-14 15:17:18 +0800

    push

--- src/creaway/dg/test/dgtest.java
+++ src/creaway/dg/test/dgtest.java
@@ -1,5 +1,11 @@
 package creaway.dg.test;
 
 public class dgtest {
+	public static void main(String[] args) {
+		System.out.print("github");
+		System.out.print("github");
+		System.out.print("~~~~~");
+		System.out.print("add by hyp");
+	}
 
 }
