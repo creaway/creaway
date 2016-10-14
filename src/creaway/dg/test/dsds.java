85cf3bdd92da8dc2800665eeec78b755520b390a
harckers <harckers@163.com>
2016-10-14 15:16:00 +0800

    test

--- src/creaway/dg/test/dsds.java
+++ src/creaway/dg/test/dsds.java
@@ -2,4 +2,17 @@ package creaway.dg.test;
 
 public class dsds {
 
+	@Override
+	public int hashCode() {
+		// add by hyp
+		return super.hashCode();
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return super.toString();
+	}
+
+	
 }
