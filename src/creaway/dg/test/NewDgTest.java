67e590e9f06e902914b9bd120dfcbb38bc1c85e0
harckers <harckers@163.com>
2016-10-14 09:30:24 +0800

    测试

---
+++ src/creaway/dg/test/NewDgTest.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package creaway.dg.test;
+
+/**
+ * @author Administrator
+ *
+ */
+public class NewDgTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
