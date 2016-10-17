7c87cf75795417143b54f60420fc3217eeede367
harckers <harckers@163.com>
2016-10-17 12:48:54 +0800

    test

---
+++ src/com/creaway/test/TestCls.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.creaway.test;
+
+/**
+ * @author Administrator
+ *
+ */
+public class TestCls {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
